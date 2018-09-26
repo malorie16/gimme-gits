3e2023b426978c1602a1d0a04da57464ed1cbca5
Malorie <malorie18@gmail.com>
2018-09-26 12:00:14 -0400

    removed save and delete

--- src/App.js
+++ src/App.js
@@ -3,81 +3,79 @@ import Search from './components/search.js'
 import GitCard from './components/GitCard.js'
 import getUser from './adapter/adapter.js'
 
+//local storage
+const store = JSON.parse(localStorage.getItem("store")) || {};
+
 class App extends Component {
   state = {
     user: '',
     favorite: '',
     toggle: ''
   }
-
+  
   //hits Github API and sets the local state to its response
   handleSubmit = (username) => {
-    getUser(username)
-    .then(data => {
+    //checks local storage before hitting API
+    if (store[username]) {
       this.setState({
-        user: data,
-        toggle: 'search'
+          favorite: JSON.parse(localStorage.store)[username],
+          toggle: 'fav'
       })
-    })
+    //if it's not in local storage, we hit the API and save it to local storage
+    } else {
+      getUser(username)
+      .then(data => {
+        this.setState({
+          user: data,
+          toggle: 'search'
+        }, () => store[username] = this.state.user)
+      })
+      localStorage.setItem('store', JSON.stringify(store))
+    }
   }
 
-  //saves user to local storage
-  handleSave = () => {
-    const store = JSON.parse(localStorage.getItem('store')) || {}
-    store[this.state.user.login] = this.state.user
-    localStorage.setItem('store', JSON.stringify(store))
-    //needed to trigger a re-render so the favorites list can update
-    this.setState({ 
-      user: this.state.user 
-    });
-  }
-
+  //renders list of favorites if they exist
   renderFavorites = () => {
-    //render favorites
     if (localStorage.store) {
     const favs = Object.keys(JSON.parse(localStorage.store)).map((key) => {
         return <p id='fav' key={key.id} onClick={this.viewFavorite}>{key}</p>
      })
-     return <div>
-       <p>Recent Favs:</p>
-       {favs}
-     </div>
+     return (
+      <div>
+        <p>Recent searches:</p>
+        {favs.reverse()}
+      </div>
+     )
     } else {
       return
     }
   }
 
+  //sets state so we can toggle from the searched user to the favorited user
   viewFavorite = (e) => {
     const fav = e.target.innerText
-    // debugger
     this.setState({
       favorite: JSON.parse(localStorage.store)[fav],
       toggle: 'fav'
     })
   }
 
-  handleDelete = (e) => {
-    // debugger
-    delete JSON.parse(localStorage.store)[this.state.favorite.login]
-    this.setState({
-      favorite: ''
-    })
+  //renders favorited user
+  renderSavedCard = () => {
+    return <GitCard user={this.state.favorite} favorite='true' deleteFavorite={this.handleDelete}/>;
   }
 
-  renderFavoriteCard = () => {
-    return <GitCard user={this.state.favorite} handleSave={this.handleSave} favorite='true' deleteFavorite={this.handleDelete}/>;
-  }
-
+  //renders searched user
   renderSearchCard = () => {
     return <GitCard user={this.state.user} handleSave={this.handleSave} />
   }
 
   render() {
-    console.log(this.state.toggle)
+    console.log(this.state.favorite)
     return (
       <div className="git-container">
         <Search handleSubmit={this.handleSubmit}/>
-        {this.state.toggle === 'fav' ? this.renderFavoriteCard() : this.renderSearchCard()}
+        {this.state.toggle === 'fav' ? this.renderSavedCard() : this.renderSearchCard()}
         {this.renderFavorites()}
       </div>
     );
--- src/components/GitCard.js
+++ src/components/GitCard.js
@@ -8,9 +8,6 @@ const GitCard = (props) => {
       return <h1>We couldn't find that user! Try again.</h1>
     //displays user's information if fetch was successful
     } else if (props.user) {
-      // console.log(JSON.parse(localStorage.getItem('user')))
-      const deleteButton = <button onClick={props.deleteFavorite}>Delete</button>
-      const saveButton = <button onClick={props.handleSave}>Save</button>;
       return (
         <div className='git-card'>
           <img src={props.user.avatar_url} alt=''/>
@@ -19,8 +16,6 @@ const GitCard = (props) => {
             <p>{props.user.public_repos} public repo(s)</p>
             <p>{props.user.email || "This user doesn't have an email on record!"}</p>
             <p>{props.user.company || "This user doesn't have a company on record!"}</p>
-            
-            {props.favorite === 'true' ? deleteButton : saveButton}
           </div>
         </div>
       )
