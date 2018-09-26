000a927fa819b12d08df74471ced3eb97d50d85c
Malorie <malorie18@gmail.com>
2018-09-26 16:19:31 -0400

    fixed key prop

--- src/components/PriorSearch.js
+++ src/components/PriorSearch.js
@@ -4,7 +4,7 @@ const PriorSearch = (props) => {
     //if the store exists in local storage render its keys
     if (localStorage.store) {
         const favs = Object.keys(JSON.parse(localStorage.store)).map((key) => {
-            return <p id='fav' key={key.id} onClick={props.viewSearched}>{key}</p>
+            return <p id='fav' key={Math.random()} onClick={props.viewSearched}>{key}</p>
         })
         return (
             <div>
